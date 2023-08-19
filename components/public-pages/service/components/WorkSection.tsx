ae2b9538662e7e5db3a4adf9292e0e071672862e
Abdul Rehman <abdulrehmanazmat007@gmail.com>
2023-08-19 17:33:56 +0500

    Added WorkSection in Service Page

--- components/public-pages/service/components/WorkSection.tsx
+++ components/public-pages/service/components/WorkSection.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import style from "@/styles/public-pages/service/service.module.css";
 
 const WorkSection = () => {
   return (
-    <>
-      <Container className="py-5">
+    <section className="off-white-background">
+      <Container className="py-5 off-white-background">
         <Row>
           <Col md={2}></Col>
           <Col md={8}>
@@ -18,8 +19,138 @@ const WorkSection = () => {
           </Col>
           <Col md={2}></Col>
         </Row>
+        <Row className="my-5">
+          {/* First 3 Cards */}
+          <Col className="px-4 my-md-2 my-3" md={4}>
+            <Row className="white-background py-lg-5 py-3">
+              <Col md={3}>
+                <div className="mb-md-0 mb-3">
+                  <img
+                    className={style?.workSectionCardImage}
+                    src="icons/water-bottle.png"
+                    alt="No Icon Found"
+                  />
+                </div>
+              </Col>
+              <Col md={9}>
+                <div>
+                  <h6 className="weight-800">Lorem ipsum dolor</h6>
+                  <p className=" text-grey weight-500">
+                    Lorem ipsum dolor sit amet consectetur.{" "}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="px-4 my-md-2 my-3" md={4}>
+            <Row className="white-background py-lg-5 py-3">
+              <Col md={3}>
+                <div className="mb-md-0 mb-3">
+                  <img
+                    className={style?.workSectionCardImage}
+                    src="icons/book.png"
+                    alt="No Icon Found"
+                  />
+                </div>
+              </Col>
+              <Col md={9}>
+                <div>
+                  <h6 className="weight-800">Lorem ipsum dolor</h6>
+                  <p className=" text-grey weight-500">
+                    Lorem ipsum dolor sit amet consectetur.{" "}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="px-4 my-md-2 my-3" md={4}>
+            <Row className="white-background py-lg-5 py-3">
+              <Col md={3}>
+                <div className="mb-md-0 mb-3">
+                  <img
+                    className={style?.workSectionCardImage}
+                    src="icons/home.png"
+                    alt="No Icon Found"
+                  />
+                </div>
+              </Col>
+              <Col md={9}>
+                <div>
+                  <h6 className="weight-800">Lorem ipsum dolor</h6>
+                  <p className=" text-grey weight-500">
+                    Lorem ipsum dolor sit amet consectetur.{" "}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          {/* Second 3 Cards */}
+          <Col className="px-4 my-md-2 my-3" md={4}>
+            <Row className="white-background py-lg-5 py-3">
+              <Col md={3}>
+                <div className="mb-md-0 mb-3">
+                  <img
+                    className={style?.workSectionCardImage}
+                    src="icons/world.png"
+                    alt="No Icon Found"
+                  />
+                </div>
+              </Col>
+              <Col md={9}>
+                <div>
+                  <h6 className="weight-800">Lorem ipsum dolor</h6>
+                  <p className=" text-grey weight-500">
+                    Lorem ipsum dolor sit amet consectetur.{" "}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="px-4 my-md-2 my-3" md={4}>
+            <Row className="white-background py-lg-5 py-3">
+              <Col md={3}>
+                <div className="mb-md-0 mb-3">
+                  <img
+                    className={style?.workSectionCardImage}
+                    src="icons/health.png"
+                    alt="No Icon Found"
+                  />
+                </div>
+              </Col>
+              <Col md={9}>
+                <div>
+                  <h6 className="weight-800">Lorem ipsum dolor</h6>
+                  <p className=" text-grey weight-500">
+                    Lorem ipsum dolor sit amet consectetur.{" "}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="px-4 my-md-2 my-3" md={4}>
+            <Row className="white-background py-lg-5 py-3">
+              <Col md={3}>
+                <div className="mb-md-0 mb-3">
+                  <img
+                    className={style?.workSectionCardImage}
+                    src="icons/home.png"
+                    alt="No Icon Found"
+                  />
+                </div>
+              </Col>
+              <Col md={9}>
+                <div>
+                  <h6 className="weight-800">Lorem ipsum dolor</h6>
+                  <p className=" text-grey weight-500">
+                    Lorem ipsum dolor sit amet consectetur.{" "}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Container>
-    </>
+    </section>
   );
 };
 
